fd3aa2c5e3003d7ad742bf0c9fdfbf9504d245b2
desk + mouse <joey.zhuang21@ycdsbk12.ca>
2019-03-28 19:01:31 -0400

    squirrels control flow practice

--- Working/2_Control_Flow/2_1_function.py
+++ Working/2_Control_Flow/2_1_function.py
@@ -1,3 +1,15 @@
+'''
+-------------------------------------------------------------------------------
+Name:		cigar_party.py
+Purpose:	to determine if the squirrel party is a cigar party
+
+Author:	Zhuang.J
+
+Created:	date in 22/03/2019
+------------------------------------------------------------------------------
+'''
+
+
 def cigar_party(cigars, is_weekend):
     if (40 <= cigars and cigars >= 60) and not (is_weekend):
         print("True")
